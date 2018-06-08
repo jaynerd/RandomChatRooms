d87e4fd06431db70d56a36a6859bbb48527dcbb0
unknown <j4yn3rd@gmail.com>
2018-06-08 15:34:44 +1200

    login logic completed

--- app/src/main/java/com/namyoon/randomchatrooms/LoginActivity.java
+++ app/src/main/java/com/namyoon/randomchatrooms/LoginActivity.java
@@ -1,15 +1,41 @@
 package com.namyoon.randomchatrooms;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText et_user;
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Login");
         setContentView(R.layout.activity_login);
 
+        et_user = (EditText) findViewById(R.id.et_user);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str_name = et_user.getText().toString();
+
+                if ("".equals(str_name)) {
+                    Toast.makeText(LoginActivity.this, "Enter you user name.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("name", str_name);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
